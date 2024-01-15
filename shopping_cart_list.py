6bc79a52036e8ee7ab85532a1634512c3bd64945
Matt Spencer <spencermatt93@gmail.com>
2024-01-15 17:25:14 +0000

    2D list added for item products, Qty, price

--- shopping_cart_list.py
+++ shopping_cart_list.py
@@ -39,29 +39,6 @@ while (not program):
         print("\n")
     print("-" * 75)
     print("This is your Shopping Cart: ")
-
-    if (len(cart) > 0):
-        # if whiskers > 1:
-        #     cart.remove(items[0])
-
-        # elif kong > 1:
-        #     cart.remove(items[1])
-
-        # elif hay > 1:
-        #     cart.remove(items[2])
-
-        # elif fish > 1:
-        #     cart.remove(items[3])
-        end = False
-        counter = 0
-        # while (not end):
-
-        #     if (counter == len(cart)-1):
-        #         end = True
-
-        #     print(f" {cart[counter]}, £{item_price[counter]}") # needs changing
-        #     counter += 1
-
     print("-" * 75)
     print("1. Add an item to your cart")
     print("2. Remove an item to your cart")
@@ -79,65 +56,30 @@ while (not program):
         item_choice = int(
             input("Please type the item from the list you wish to add: "))
 
-        for i in range(5):  # changed from range 5
-            if items[i-1] not in cart:
+        for i in range(len(items)):  # changed from below "Old code" Works!
+            if items[i] not in cart:
+                if item_choice == i+1:
+                    cart.append(items[i])
+                    print(f"You have added 1x {items[i][0]}")
+                    if items[i][0] == cart[-1][0]:
+                        cart[-1][1] += 1
+                        item_count += 1
+                        break
 
-                if item_choice == i:
-                    cart.append(items[i-1])
-                    item_count += 1
 
-                    if item_choice == 1:
-                        print("You have added 1x Whiskers")
-                        whiskers += 1
-                        items[0][1] = whiskers
-                        break
-                    elif item_choice == 2:
-                        print("You have added 1x Kong")
-                        kong += 1
-                        items[1][1] = kong
-                        break
-                    elif item_choice == 3:
-                        print("You have added 1x Hay")
-                        hay += 1
-                        items[2][1] = hay
+# doesnt work yet - need conditonal to seperate this
+            elif items[i] in cart and item_choice == i+1:
 
-                    elif item_choice == 4:
-                        print("You have added 1x Fish Food")
-                        fish += 1
-                        items[3][1] = fish
+                # for y in range(len(items)):
+                #     if cart[y][1] > 0:
 
-            elif items[0] in cart and item_choice == 1:
                 for x in cart:
-                    if items[0][0] in x:  # if whiskers in cart
-                        index_1 = cart.index(x)
-                        index_2 = x.index("Whiskers")
-                        cart[index_1][index_2 + 1] += 1
-                        break
-                break
-            elif items[1] in cart and item_choice == 2:
-                for x in cart:
-                    if items[1][0] in x:  # if Kong in cart
-                        index_1 = cart.index(x)
-                        index_2 = x.index("Kong")
-                        cart[index_1][index_2 + 1] += 1
-                        break
-                break
-            elif items[2] in cart and item_choice == 3:
-                for x in cart:
-                    if items[2][0] in x:  # if Kong in cart
-                        index_1 = cart.index(x)
-                        index_2 = x.index("Hay")
-                        cart[index_1][index_2 + 1] += 1
-                        break
-                break
-            elif items[3] in cart and item_choice == 4:
-                for x in cart:
-                    if items[3][0] in x:  # if Kong in cart
-                        index_1 = cart.index(x)
-                        index_2 = x.index("Fish Food")
-                        cart[index_1][index_2 + 1] += 1
-                        break
-                break
+                    if item_choice == i + 1:
+                        if items[i] == x:
+                            index_1 = cart.index(x)
+                            cart[index_1][1] += 1
+                            item_count += 1
+                            break
 
     if menu_choice == 2:
         print(" Please choose which item you want to remove: ")
